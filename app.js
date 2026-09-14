a758690dc711ef65f1d34efa9d0527f3e7e73f52
Souvik Das <souvikdas1224@gmail.com>
2026-09-14 20:08:52 +0530

    hahahah

---
+++ app.js
@@ -0,0 +1,10 @@
+const express = require("express");
+const app = express();
+
+app.get("/", (req, res) => {
+    res.send("Hi, I am root");
+});
+
+app.listen(8080, () => {
+    console.log("server is listening to port 8080");
+});
